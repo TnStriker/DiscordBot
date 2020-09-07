5c86c398cb811dd4d3b001598b88ade3838c340b
TnStriker <djsteve71234@outlook.com>
2020-09-06 22:36:59 -0700

    Delete members.js

--- commands/members.js
+++
@@ -1,9 +0,0 @@
-module.exports= {
-    name: 'server',
-    description: "This display how many people are in the server",
-    execute(message, args){
-        message.channel.send('Calculating total members.....').then(msg => {
-            msg.edit(`**Server:** ${message.guild.name}\n**Total members:** ${message.guild.memberCount}`);
-        })
-    }
-}
