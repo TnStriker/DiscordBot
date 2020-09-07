d1cbb12ee27d8a4ef5711fa173b4107f501fd132
TnStriker <djsteve71234@outlook.com>
2020-09-06 22:37:15 -0700

    Update youtube.js

--- commands/youtube.js
+++ commands/youtube.js
@@ -4,13 +4,13 @@ module.exports= {
     execute(message, args) {
 
         if(message.member.roles.cache.has('674369573495111690')){
-            message.channel.send('Check out my Youtube Channel! https://www.youtube.com/channel/UCVi5FT5cseYIOvi38PYo4tg?')
+            message.channel.send('Check out my Youtube Channel! https://www.youtube.com/channel/UCVi5FT5cseYIOvi38PYo4tg')
         
         } else if(message.member.roles.cache.has('167451970855108609')){
-                message.channel.send('Check out my Youtube Channel! https://www.youtube.com/channel/UCVi5FT5cseYIOvi38PYo4tg?')
+                message.channel.send('Check out my Youtube Channel! https://www.youtube.com/channel/UCVi5FT5cseYIOvi38PYo4tg')
 
         } else if(message.member.roles.cache.has('167451506927206400')){
-                message.channel.send('Check out my Youtube Channel! https://www.youtube.com/channel/UCVi5FT5cseYIOvi38PYo4tg?')        
+                message.channel.send('Check out my Youtube Channel! https://www.youtube.com/channel/UCVi5FT5cseYIOvi38PYo4tg')        
 
 
         } else {
