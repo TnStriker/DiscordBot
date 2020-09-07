c7f1eb62bc615e30a2cba5531948621585ce6ca0
TnStriker <djsteve71234@outlook.com>
2020-09-06 22:37:24 -0700

    Update messageUpdate.js

--- events/guild/messageUpdate.js
+++ events/guild/messageUpdate.js
@@ -1,18 +1,19 @@
 const {MessageEmbed} = require('discord.js')
-module.exports=async(oldMessage,newMessage)=>{
+module.exports = async(oldMessage, newMessage)=>{
 
     let embed = new MessageEmbed()
     .setAuthor(oldMessage.author.tag, oldMessage.author.avatarURL)
     .setThumbnail(oldMessage.author.displayAvatarURL())
     .setColor('GREEN')
     .setTitle('A Message has been Edited!')
-    .addField('Before:', oldMessage.content,true)
+    .addField('Before:', oldMessage.content, true)
     .addField('After:', newMessage.content, true)
     .setTimestamp()
     .setFooter("StrikerBot doing it's job :D")
     
     let channel = oldMessage.guild.channels.cache.find(ch => ch.name === "botspam")
     if(!channel) return;
+    if (message.author.bot) return;
     channel.send(embed)
 
 }
