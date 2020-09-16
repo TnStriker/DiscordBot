875997c07d34e9c150779ec4687fe86ba8d4e81b
TnStriker <djsteve71234@outlook.com>
2020-09-15 23:33:54 -0700

    Update server.js

--- commands/server.js
+++ commands/server.js
@@ -1,9 +1,24 @@
+const Discord = require('discord.js')
+
 module.exports= {
     name: 'server',
     description: "This display how many people are in the server",
     execute(message, args){
         message.channel.send('Calculating total members.....').then(msg => {
-            msg.edit(`**Server:** ${message.guild.name}\n**Total members:** ${message.guild.memberCount}`);
+
+            const logo = 'https://static-cdn.jtvnw.net/jtv_user_pictures/04ef79cf-11cb-4294-9c3a-b9ab65a7467c-profile_image-300x300.png'
+            
+            const embed = new Discord.MessageEmbed()
+            .setColor('BLUE')
+            //Can change the name and url image of the Author here
+            .setAuthor('StrikerBot Tester', logo)
+            .addField('**Server:**', `${message.guild.name}`)
+            .setThumbnail(logo)
+            .addField('**Total Members:**', `${message.guild.memberCount}`)
+            .setTimestamp()
+            .setFooter("StrikerBot doing it's job :D")
+
+            msg.edit('\u200b',embed);
         })
     }
 }
