327b10ed18c55be326f596f5fe02b31999872af0
TnStriker <djsteve71234@outlook.com>
2020-09-15 23:34:36 -0700

    Update messageUpdate.js

--- events/guild/messageUpdate.js
+++ events/guild/messageUpdate.js
@@ -6,14 +6,14 @@ module.exports = async(oldMessage, newMessage)=>{
     .setThumbnail(oldMessage.author.displayAvatarURL())
     .setColor('GREEN')
     .setTitle('A Message has been Edited!')
-    .addField('Before:', oldMessage.content, true)
-    .addField('After:', newMessage.content, true)
+    .addField('Before:', `${oldMessage.content}`,true)
+    .addField('After:', `${newMessage.content}`,true)
     .setTimestamp()
     .setFooter("StrikerBot doing it's job :D")
     
+    //Channel Name
     let channel = oldMessage.guild.channels.cache.find(ch => ch.name === "botspam")
     if(!channel) return;
-    if (message.author.bot) return;
+    //if (message.author.bot) return;
     channel.send(embed)
-
 }
