e4401b959b7c0e50fe02ba2e2807391b58c1b5cc
TnStriker <djsteve71234@outlook.com>
2020-09-15 23:34:23 -0700

    Create guildBanAdd.js

---
+++ events/guild/guildBanAdd.js
@@ -0,0 +1,17 @@
+const {MessageEmbed} = require('discord.js')
+module.exports = async(guild, user)=>{
+
+    let embed = new MessageEmbed()
+    .setTitle('User has been BANNED')
+    .setColor('BLACK')
+    .setThumbnail(user.displayAvatarURL())
+    .addField(`User banned from ${guild.name}:`, user.tag)
+    .setTimestamp()
+    .setFooter("StrikerBot took out the trash! -_-")
+    
+    //Channel Name
+    let channel = member.guild.channels.cache.find(ch => ch.name === "botspam")
+    if(!channel) return;
+    channel.send(embed)
+
+}
