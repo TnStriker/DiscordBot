421fb39f3df69ce3022f32d3c6604d8522a2953c
TnStriker <djsteve71234@outlook.com>
2020-09-15 23:34:27 -0700

    Update messageDeleted.js

--- events/guild/messageDeleted.js
+++ events/guild/messageDeleted.js
@@ -5,12 +5,13 @@ module.exports = async(message)=>{
     .setTitle('A Message has been Deleted!')
     .setColor('RED')
     .setThumbnail(message.author.displayAvatarURL())
-    .addField('Deleted By:', message.author.tag)
-    .addField('Deleted Message:', message.content,true)
-    .addField('Deleted in:', message.channel)
-    .addField('Deleted at:', message.createdAt)
+    .addField('Deleted By:', `${message.author.tag}`)
+    .addField('Deleted Message:', `${message.content}`,true)
+    .addField('Deleted in:', `${message.channel}`)
+    .addField('Deleted at:', `${message.createdAt}`)
     .setFooter("StrikerBot doing it's job :D")
 
+    //Channel Name
     let channel = message.guild.channels.cache.find(ch => ch.name === "botspam")
     if(!channel) return;
     if (message.author.bot) return;
