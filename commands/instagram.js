de300edd77f5283599e14c26aa92bd0c28bf03a6
TnStriker <djsteve71234@outlook.com>
2020-09-15 23:33:24 -0700

    Update instagram.js

--- commands/instagram.js
+++ commands/instagram.js
@@ -1,7 +1,21 @@
+const Discord = require('discord.js')
+
 module.exports= {
     name: 'instagram',
     description: "Sends link of Instagram Account",
     execute(message, args) {
-        message.channel.send('Check out my Instagram Profile! https://www.instagram.com/_TnStriker_');
+
+        const logo = 'https://cdn.discordapp.com/attachments/674365739918163968/752702126903525436/tnstriker2-chibi.png'
+
+        const embed = new Discord.MessageEmbed() 
+        .setAuthor('Instagram')
+        .setTitle('TnStriker')
+        .setURL('https://www.instagram.com/_TnStriker_')
+        .setImage(logo)
+        .setTimestamp()
+        .setColor('ORANGE')
+        .setFooter("StrikerBot doing it's job :D")
+        
+        message.channel.send(embed)
     }
 }
