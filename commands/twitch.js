500ae2f364ab68d895cde0f33ab91522f5b648dd
TnStriker <djsteve71234@outlook.com>
2020-09-15 23:33:58 -0700

    Update twitch.js

--- commands/twitch.js
+++ commands/twitch.js
@@ -1,22 +1,21 @@
+const Discord = require('discord.js')
+
 module.exports= {
     name: 'twitch',
     description: "Sends link of Twitch Channel",
     execute(message, args) {
 
-        if(message.member.roles.cache.has('674369573495111690')){
-            message.channel.send('Check out my Twitch Channel! https://www.twitch.tv/TnStrikerGamer')
+        const logo = 'https://yt3.ggpht.com/a/AATXAJy6XbekbgDLQumtbXeiWfKf2ujkqYrps5cNdpAgNA=s900-c-k-c0xffffffff-no-rj-mo'
 
-        } else if(message.member.roles.cache.has('167451970855108609')){
-                message.channel.send('Check out my Twitch Channel! https://www.twitch.tv/TnStrikerGamer')
+        const embed = new Discord.MessageEmbed() 
+        .setAuthor('Twitch')
+        .setTitle('TnStrikerGamer')
+        .setURL('https://www.twitch.tv/TnStrikerGamer')
+        .setImage(logo)
+        .setTimestamp()
+        .setColor('PURPLE')
+        .setFooter("StrikerBot doing it's job :D")
 
-        } else if(message.member.roles.cache.has('167451506927206400')){
-                message.channel.send('Check out my Twitch Channel! https://www.twitch.tv/TnStrikerGamer')        
-
-
-        } else {
-            message.channel.send('Hey there bud. You do not have the proper permissions to use this command!');
-        }
-
-
+        message.channel.send(embed)
     }
 }
