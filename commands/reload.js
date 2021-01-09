27b04f3e45c9a1bedd93b2a40297d573ebb1c7c4
TnStriker <djsteve71234@outlook.com>
2021-01-08 17:48:24 -0800

    Create reload.js

---
+++ commands/reload.js
@@ -0,0 +1,29 @@
+module.exports = {
+    name: 'reload',
+    aliases: ['reload'],
+	description: 'Reloads a command',
+	execute(message, args) {
+        // ...
+            
+            if(!message.member.roles.cache.has('167451506927206400')) return message.channel.send("You are not a mod of the server.");
+
+            if (!args.length) return message.channel.send(`You didn't pass any command to reload, ${message.author}!`);
+    const commandName = args[0].toLowerCase();
+    const command = message.client.commands.get(commandName)
+	    || message.client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+
+            if (!command) return message.channel.send(`There is no command with name or alias \`${commandName}\`, ${message.author}!`);
+
+                delete require.cache[require.resolve(`./${command.name}.js`)];
+
+                try {
+                    const newCommand = require(`./${command.name}.js`);
+                    message.client.commands.set(newCommand.name, newCommand);
+                } catch (error) {
+                    console.error(error);
+                    message.channel.send(`There was an error while reloading a command \`${command.name}\`:\n\`${error.message}\``);
+                }
+
+                message.channel.send(`Command \`${command.name}\` was reloaded!`);
+	},
+};
