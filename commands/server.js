926daa5c251bde45b359d0a42a3e283ac317af72
TnStriker <djsteve71234@outlook.com>
2021-01-08 17:48:34 -0800

    Update server.js

--- commands/server.js
+++ commands/server.js
@@ -4,21 +4,18 @@ module.exports= {
     name: 'server',
     description: "This display how many people are in the server",
     execute(message, args){
-        message.channel.send('Calculating total members.....').then(msg => {
-
-            const logo = 'https://static-cdn.jtvnw.net/jtv_user_pictures/04ef79cf-11cb-4294-9c3a-b9ab65a7467c-profile_image-300x300.png'
+        ///Image link of Author
+        const logo = 'https://static-cdn.jtvnw.net/jtv_user_pictures/04ef79cf-11cb-4294-9c3a-b9ab65a7467c-profile_image-300x300.png'
             
-            const embed = new Discord.MessageEmbed()
-            .setColor('BLUE')
-            //Can change the name and url image of the Author here
-            .setAuthor('StrikerBot Tester', logo)
-            .addField('**Server:**', `${message.guild.name}`)
-            .setThumbnail(logo)
-            .addField('**Total Members:**', `${message.guild.memberCount}`)
-            .setTimestamp()
-            .setFooter("StrikerBot doing it's job :D")
-
-            msg.edit('\u200b',embed);
-        })
+        const embed = new Discord.MessageEmbed()
+        .setColor('BLUE')
+        //Can change the name and url image of the Author here
+        .setAuthor('StrikerBot Tester', logo)
+        .addField('**Server:**', `${message.guild.name}`)
+        .setThumbnail(logo)
+        .addField('**Total Members:**', `${message.guild.memberCount}`)
+        .setTimestamp()
+        .setFooter("StrikerBot doing it's job :D")
+        message.channel.send(embed)
     }
 }
