3aefd8cb530a4082a9f184ab5557854aa8bbc3b4
TnStriker <djsteve71234@outlook.com>
2021-04-23 14:26:26 -0700

    Fixed reload utility.

--- commands/Utility/reload.js
+++ commands/Utility/reload.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 module.exports = {
     name: 'reload',
     aliases: ['reload'],
@@ -5,19 +6,23 @@ module.exports = {
 	execute(message, args) {
         // ...
             
-            if(!message.member.roles.cache.has('167451506927206400')) return message.channel.send("You are not a mod of the server.");
+            if(!message.member.roles.cache.has('**')) return message.channel.send("You are not a mod of the server.");
 
             if (!args.length) return message.channel.send(`You didn't pass any command to reload, ${message.author}!`);
-    const commandName = args[0].toLowerCase();
-    const command = message.client.commands.get(commandName)
-	    || message.client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+            
+            const commandName = args[0].toLowerCase();
+            const command = message.client.commands.get(commandName)
+	            || message.client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
 
             if (!command) return message.channel.send(`There is no command with name or alias \`${commandName}\`, ${message.author}!`);
 
-                delete require.cache[require.resolve(`./${command.name}.js`)];
+            const commandFolders = fs.readdirSync('./commands');
+            const folderName = commandFolders.find(folder => fs.readdirSync(`./commands/${folder}`).includes(`${command.name}.js`));
+                
+                delete require.cache[require.resolve(`../${folderName}/${command.name}.js`)];
 
                 try {
-                    const newCommand = require(`./${command.name}.js`);
+                    const newCommand = require(`../${folderName}/${command.name}.js`);
                     message.client.commands.set(newCommand.name, newCommand);
                 } catch (error) {
                     console.error(error);
